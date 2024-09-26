a1bc5514132d4f6d62761b9ba1ae37e30a05fafc
Jose Conchello <jose.conchello@gmail.com>
2024-09-26 15:32:28 -0700

    save camera rotation work, WIP figure out how to reset correctly

--- Assets/CityEngine/Assets/Scripts/Utilities/Numbers.cs
+++ Assets/CityEngine/Assets/Scripts/Utilities/Numbers.cs
@@ -5,12 +5,10 @@ public static class NumbersUtils
     // Remap function to remap values from one range to another
     public static float Remap(float inputMin, float inputMax, float outputMin, float outputMax, float value)
     {
-        // Normalize the input value to a 0-1 range using Mathf.InverseLerp
+        // Normalize the input value to a range using Mathf.InverseLerp
         float t = Mathf.InverseLerp(inputMin, inputMax, value);
 
         // Remap the normalized value to the output range using Mathf.Lerp
         return Mathf.Lerp(outputMin, outputMax, t);
     }
-
-    // You can add more utility functions here as needed
 }
